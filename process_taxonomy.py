d5e9b4743d134d9bd30cad3e213985d91cf28997
Carlos Ferreira <carlosferreira@outlook.com>
2020-04-13 20:06:08 +0100

    Taxonomy processing script

--- process_taxonomy.py
+++ process_taxonomy.py
@@ -1,17 +1,27 @@
 import pandas as pd
-import numpy as np
+import time
 
 pd.set_option('display.max_columns', None)
 
 raw = pd.read_csv('raw/taxonomy.csv')
 
+# >>> raw.columns # ALL COLUMNS
+# >>> raw.columns
+# Index(['internalTaxonId', 'scientificName', 'kingdomName', 'phylumName',
+#        'orderName', 'className', 'familyName', 'genusName', 'speciesName',
+#        'infraType', 'infraName', 'infraAuthority', 'subpopulationName',
+#        'authority', 'taxonomicNotes'],
+#       dtype='object')
+
 # Remove the following "clutter columns"
 to_drop = [
+    'infraType', 'infraName', 'infraAuthority', 'subpopulationName',
+    'authority', 'taxonomicNotes'
 ]
 raw.drop(columns = to_drop, inplace = True)
 
 print(raw.head())
 
-
 # Save new processed version for upload to HDFS
-raw.to_csv('processed/taxonomy.csv', index = False)
+timestr = time.strftime("%Y%m%d-%H%M%S")
+raw.to_csv('processed/taxonomy-' + timestr + '.csv', index = False)
