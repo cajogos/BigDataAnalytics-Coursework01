6ef2be4bec8415ac10353650cb702374d2756310
Carlos Ferreira <carlosferreira@outlook.com>
2020-04-20 14:06:53 +0100

    Added Google Maps to page

--- web/static/js/classes/MapDisplay.js
+++ web/static/js/classes/MapDisplay.js
@@ -2,8 +2,5 @@
 APP.MapDisplay = function(element)
 {
     this._element = $(element);
-
-    this._element.css('background', 'lightblue');
-    this._element.text('MAP GOES HERE');
 };
 APP.MapDisplay.prototype.constructor = APP.MapDisplay;
